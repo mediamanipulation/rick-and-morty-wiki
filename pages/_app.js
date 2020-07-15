3024f32958b34d12895e11b601504a87f55e0135
mediamanipulation <manipulation2000@hotmail.com>
2020-07-15 10:17:36 -0500

    init

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+// In pages/_app.js
+import { motion, AnimatePresence } from 'framer-motion';
+function App({ Component, pageProps, router }) {
+    return (
+        <AnimatePresence>
+            <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
+                pageInitial: {
+                    opacity: 0
+                },
+                pageAnimate: {
+                    opacity: 1
+                },
+                pageExit: {
+                    backgroundColor: 'white',
+                    filter: `invert()`,
+                    opacity: 0
+                }
+            }}>
+                <Component {...pageProps} />
+            </motion.div>
+            </AnimatePresence>
+    )
+  }
+  
+  export default App;
---
+++ pages/index.js
@@ -0,0 +1,330 @@
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import Link from 'next/link'
+import Head from 'next/head'
+
+const defaultEndpoint = `https://rickandmortyapi.com/api/character/`;
+
+export async function getServerSideProps() {
+  const res = await fetch(defaultEndpoint)
+  const data = await res.json();
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+export default function Home({ data }) {
+  const { info, results: defaultResults = [] } = data;
+  const [results, updateResults] = useState(defaultResults);
+
+  const [page, updatePage] = useState({
+    ...info,
+    current: defaultEndpoint
+  });
+
+  console.log('data', data);
+
+  const { current } = page;
+
+  useEffect(() => {
+    if (current === defaultEndpoint) return;
+
+    async function request() {
+      const res = await fetch(current)
+      const nextData = await res.json();
+
+      updatePage({
+        current,
+        ...nextData.info
+      });
+
+      if (!nextData.info?.prev) {
+        updateResults(nextData.results);
+        return;
+      }
+
+      updateResults(prev => {
+        return [
+          ...prev,
+          ...nextData.results
+        ]
+      });
+    }
+
+    request();
+  }, [current]);
+
+  function handleLoadMore() {
+    updatePage(prev => {
+      return {
+        ...prev,
+        current: page?.next
+      }
+    });
+  }
+
+  function handleOnSubmitSearch(e) {
+    e.preventDefault();
+
+    const { currentTarget = {} } = e;
+    const fields = Array.from(currentTarget?.elements);
+    const fieldQuery = fields.find(field => field.name === 'query');
+
+    const value = fieldQuery.value || '';
+    const endpoint = `https://rickandmortyapi.com/api/character/?name=${value}`;
+
+    updatePage({
+      current: endpoint
+    });
+  }
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Rick and Morty Wiki</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <motion.div initial="hidden" animate="visible" variants={{
+          hidden: {
+            scale: .8,
+            opacity: 0
+          },
+          visible: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+              delay: .4
+            }
+          },
+        }}>
+          <h1 className="title">
+            Wubba Lubba Dub Dub!
+          </h1>
+        </motion.div>
+
+        <p className="description">
+          Rick and Morty Character Wiki
+        </p>
+
+        <form className="search" onSubmit={handleOnSubmitSearch}>
+          <input name="query" type="search" />
+          <button>Search</button>
+        </form>
+
+        <ul className="grid">
+          {results.map(result => {
+            const { id, name, image } = result;
+            return (
+              <motion.li key={id} className="card" whileHover={{
+                position: 'relative',
+                zIndex: 1,
+                filter: [
+                  'hue-rotate(0) contrast(100%)',
+                  'hue-rotate(360deg) contrast(200%)',
+                  'hue-rotate(45deg) contrast(300%)',
+                  'hue-rotate(0) contrast(100%)'
+                ],
+                background: 'white',
+                scale: [1, 1.4, 1.2],
+                rotate: [0, 10, -10, 0],
+                transition: {
+                  duration: .2
+                }
+
+              }}>
+                <Link href="/character/[id]" as={`/character/${id}`}>
+                  <a>
+                    <img src={image} alt={`${name} Thumbnail`} />
+                    <h3>{name}</h3>
+                  </a>
+                </Link>
+              </motion.li>
+            )
+          })}
+        </ul>
+        <p>
+          <button onClick={handleLoadMore}>Load More</button>
+        </p>
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a>
+      </footer>
+
+      <style>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+
+          max-width: 800px;
+          margin-top: 3rem;
+          list-style: none;
+          margin-left: 0;
+          padding-left: 0;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+        .search input {
+          margin-right: .5em;
+        }
+
+        @media (max-width: 600px) {
+          .search input {
+            margin-right: 0;
+            margin-bottom: .5em;
+          }
+
+          .search input,
+          .search button {
+            width: 100%;
+          }
+          }
+      `}</style>
+    </div>
+  )
+}
